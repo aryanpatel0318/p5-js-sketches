5997b9e355d18b1be259f7510b9f799149d0a907
aryanpatel0318 <99696854+aryanpatel0318@users.noreply.github.com>
2024-05-29 14:16:36 -0400

    Create sketch1.js

---
+++ sketch1.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    fill('blue');
+    ellipse(100, 100, 50, 50);
+  } else if (keyCode === RIGHT_ARROW) {
+    fill('green');
+    ellipse(200, 200, 50, 50);
+  } else if (keyCode === 32) { 
+    fill('red');
+    ellipse(mouseX, mouseY, 75, 75);
+  }
+}
